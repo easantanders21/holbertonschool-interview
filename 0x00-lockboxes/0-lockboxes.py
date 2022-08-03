6cb1f3ffdfabc10a1734af04080be17241852d37
easantanders21 <3823@holbertonschool.com>
2022-08-03 14:15:54 -0500

    fix task 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,31 +2,21 @@
 """ lockboxes challenge """
 
 
-def check_box(dic_box):
-    """ check if there are any open boxes """
-    list_keys = []
-    for k, v in dic_box.items():
-        if "t" in v:
-            list_keys = v["t"]
-            for i in list_keys:
-                if dic_box[i] != "ok" and "f" in dic_box[i]:
-                    dic_box[i]["t"] = dic_box[i]["f"]
-                    del dic_box[i]["f"]
-            dic_box[k] = "ok"
-
-
 def canUnlockAll(boxes):
-    """ function that unlocks boxes """
-    status = True
-    number_boxes = len(boxes)
-    count = 0
-    dic_box = {0: {"t": boxes[0]}}
-    for i in range(1, number_boxes):
-        dic_box[i] = {"f": boxes[i]}
-    while count <= number_boxes:
-        count += 1
-        check_box(dic_box)
-    for k, v in dic_box.items():
-        if v != "ok":
-            status = False
-    return status
+    """ function that open boxes """
+    all_keys = []
+    for i in range(len(boxes)):
+        all_keys.append(i)
+
+    recover_keys = []
+    recover_keys.append(0)
+
+    for key in recover_keys:
+        for new_key in boxes[key]:
+            if ((new_key in all_keys) and (new_key not in recover_keys)):
+                recover_keys.append(new_key)
+            else:
+                continue
+        if all_keys == sorted(recover_keys):
+            return True
+    return False
