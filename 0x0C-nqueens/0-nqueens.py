5f10b7ad2bd88245705a8f92409246df0a60cc6c
easantanders21 <3823@holbertonschool.com>
2022-11-03 21:52:12 -0500

    task 0

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,35 +1,43 @@
 #!/usr/bin/python3
-"""0-nqueens
-contains a program that solves the N queens problem"""
+'''
+Module that solves the N Queens puzzle
+'''
 from sys import argv, exit
 
-
-def solve(N, i=0, a=[], b=[], c=[]):
-    """Generates backtracking solutions for N queens problem"""
-    if i < N:
-        for j in range(N):
-            if j not in a and i+j not in b and i-j not in c:
-                for solution in solve(N, i+1, a+[j], b+[i+j], c+[i-j]):
-                    yield solution
-    else:
-        yield a
-
-
 if __name__ == "__main__":
-
-    if not len(argv) == 2:
-        print("Usage: nqueens N")
+    if len(argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
+    try:
+        n = int(argv[1])
+    except BaseException:
+        print('N must be a number')
+        exit(1)
+    if n < 4:
+        print('N must be at least 4')
         exit(1)
 
-    if not (argv[1]).isdigit():
-        print("N must be a number")
-        exit(1)
+    solution = []
 
-    N = int(argv[1])
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
+    def solve_queens(row, n, solution):
+        if (row == n):
+            print(solution)
+        else:
+            for col in range(n):
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
 
-for solution in solve(N):
-    ans = [[c, r] for c, r in enumerate(solution)]
-    print(ans)
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+                return False
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+                return False
+        return True
+
+    solve_queens(0, n, solution)
